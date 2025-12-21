d3141c2f4131613d63e8d12c510b0735868c2673
smahuzaifa <huzaifasadath@gmail.com>
2025-12-21 12:51:04 +1100

    Android working on MACOS

--- src/test/java/appium/BaseTest2.java
+++ src/test/java/appium/BaseTest2.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import java.io.File;
@@ -18,20 +19,35 @@ public class BaseTest2 {
     @BeforeClass
     public void configuringAppium() throws URISyntaxException, MalformedURLException {
         //To Start the Appium Server automatically
-        service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users" +
-                        "\\huzai\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-                .withIPAddress("127.0.0.1").usingPort(4723).build();
-        service.start();
+        String os = System.getProperty("os.name").toLowerCase();
+        String appiumJSPath;
+
+        if (os.contains("win")) {
+            appiumJSPath = "C:\\Users\\huzai\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+        } else if (os.contains("mac")) {
+            appiumJSPath = "/opt/homebrew/lib/node_modules/appium/build/lib/main.js";
+        } else {
+            throw new RuntimeException("Unsupported OS: " + os);
+        }
+
+        service = AppiumDriverLocalService.buildService(
+                new AppiumServiceBuilder()
+                        .withAppiumJS(new File(appiumJSPath))
+                        .withIPAddress("127.0.0.1")
+                        .usingPort(4723)
+                        .withArgument(GeneralServerFlag.BASEPATH, "/wd/hub")
+        );
+        //service.start();
         //This mentions the path to main.js which starts the server and then passing the appium server
         // url and then the port number
 
         //Setting the device and its capabilites
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Pixel 9 Pro"); //Emulator
-        options.setApp("C:\\Users\\huzai\\IdeaProjects\\Appium\\src\\test\\java\\resources\\General-Store.apk");
+        options.setApp("/Users/huzaifa/IdeaProjects/AppiumPOM/src/test/java/resources/General-Store.apk");
 
         //Creating androidDriver object
-        driver = new AndroidDriver(new URI("http://127.0.0.1:4723/").toURL(),options);
+        driver = new AndroidDriver(new URI("http://127.0.0.1:4723/wd/hub").toURL(),options);
         // Uniform Resource Identifier (URI) is a string that identifies a resource, while a
         // Uniform Resource Locator (URL) is a type of URI that specifies both the identity and the
         // location of a resource, typically on the web
--- src/test/java/appium/EndToEndApplicationAndroid.java
+++ src/test/java/appium/EndToEndApplicationAndroid.java
@@ -9,25 +9,35 @@ import pageObjects.android.ProductCatalogue;
 
 public class EndToEndApplicationAndroid extends BaseTest2 {
     private FormPage fp;
-    @BeforeMethod
-    public void setActivity() {
-        fp = new FormPage(driver);
-        fp.preSetup();
-    }
+//    @BeforeMethod
+//    public void setActivity() {
+//        fp = new FormPage(driver);
+//        fp.preSetup();
+//    }
 
     @Test(dataProvider = "getData")
     public void FirstPage(String name, String country, String gender){
-//        FormPage fp = new FormPage(driver);
+       FormPage fp = new FormPage(driver);
 //        @BeforeMethod creates a FormPage object and stores it in the field fp, so when FirstPage runs right after,
 //                it can reuse that same fp without calling new FormPage(driver) again.
         fp.setNameField(name);
         fp.setCountry(country).click();
         fp.setGender(gender);
         fp.setLetsShopButton();
+        fp.preSetup();
+    }
+    @Test
+    public void FirstPage2(){
+        FormPage fp = new FormPage(driver);
+        fp.setNameField("Hello");
+        fp.setCountry("Bermuda").click();
+        fp.setGender("Female");
+        fp.setLetsShopButton();
+        fp.preSetup();
     }
     @Test
     public void addToCart(){
-        //FirstPage();
+        FirstPage2();
         String shoe = "Air Jordan 9 Retro";
         ProductCatalogue pc = new ProductCatalogue(driver);
         pc.scrollIntoView(shoe);
@@ -40,7 +50,7 @@ public class EndToEndApplicationAndroid extends BaseTest2 {
     }
     @DataProvider //When we want to test same test with different types of data
     public Object[][] getData(){
-        return new Object[][]{{"Hello","Bermuda","Female"},{"Hello World!","Argentina","Male"}};
+        return new Object[][]{{"Hello","Bermuda","Female"},{"Hello World!","Bermuda","Male"}};
         //passing multiple data
     }
 }
